86fd3314812f8c535f665ad23e0a2501aa90a2fe
tiago <ti.almeida58@gmail.com>
2024-12-26 09:48:53 -0300

    raw: first commit

---
+++ src/main/java/br/com/challengerbackendhitclean/ChallengerBackEndHitCleanApplication.java
@@ -0,0 +1,13 @@
+package br.com.challengerbackendhitclean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengerBackEndHitCleanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChallengerBackEndHitCleanApplication.class, args);
+    }
+
+}
